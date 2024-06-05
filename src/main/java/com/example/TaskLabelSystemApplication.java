bea930c17652ef32a1716eaccf2bd34ab0f50246
Soner <syln.soner@gmail.com>
2024-06-05 20:25:05 +0300

    Add user creation functionality in TaskLabelSystemApplication

--- src/main/java/com/example/TaskLabelSystemApplication.java
+++ src/main/java/com/example/TaskLabelSystemApplication.java
@@ -1,14 +1,26 @@
 package com.example;
 
+import com.example.models.Role;
+import com.example.services.concretes.UserService;
+import com.example.services.dtos.requests.AddUserRequest;
 import org.modelmapper.ModelMapper;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 
+import java.util.Set;
+
 @SpringBootApplication
-public class TaskLabelSystemApplication {
+public class TaskLabelSystemApplication implements CommandLineRunner {
+
+    private final UserService userService;
+
+    public TaskLabelSystemApplication(UserService userService) {
+        this.userService = userService;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(TaskLabelSystemApplication.class, args);
@@ -22,4 +34,35 @@ public class TaskLabelSystemApplication {
     public JavaMailSender javaMailSender() {
         return new JavaMailSenderImpl();
     }
+
+    @Override
+    public void run(String... args) throws Exception {
+        createAdmin();
+    }
+
+    private void createAdmin() {
+        AddUserRequest request = AddUserRequest.builder()
+                .username("admin")
+                .password("admin")
+                .email("syln.soner@gmail.com")
+                .authorities(Set.of(Role.ROLE_ADMIN))
+                .build();
+
+        AddUserRequest request2 = AddUserRequest.builder()
+                .username("soner")
+                .password("soner")
+                .email("syln.soner@gmail.com")
+                .authorities(Set.of(Role.ROLE_USER))
+                .build();
+
+        if (userService.getByUserName(request.getUsername()).isEmpty()) {
+            userService.createUser(request);
+        }
+
+        if (userService.getByUserName(request2.getUsername()).isEmpty()) {
+            userService.createUser(request2);
+        }
+    }
+
+
 }
