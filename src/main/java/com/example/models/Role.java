fca3541d9c7379402cf56eb2302f4f1b8bd3d489
Soner <syln.soner@gmail.com>
2024-06-05 20:24:50 +0300

    Add enum for user roles

--- src/main/java/com/example/models/Role.java
+++ src/main/java/com/example/models/Role.java
@@ -1,2 +1,23 @@
-package com.example.models;public class Role {
+package com.example.models;
+
+import org.springframework.security.core.GrantedAuthority;
+
+public enum Role implements GrantedAuthority {
+    ROLE_USER("USER"),
+    ROLE_ADMIN("ADMIN");
+
+    private String value;
+
+    Role(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    @Override
+    public String getAuthority() {
+        return name();
+    }
 }
