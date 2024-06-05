32a84a050df8be5faba7ca30d45fe1f24b12a733
Soner <syln.soner@gmail.com>
2024-06-05 20:25:14 +0300

    Refactor controllers and add CRUD operations

--- src/main/java/com/example/controllers/LabelController.java
+++ src/main/java/com/example/controllers/LabelController.java
@@ -1,2 +1,50 @@
-package com.example.controllers;public class LabelController {
+package com.example.controllers;
+
+import com.example.core.utilities.results.DataResult;
+import com.example.core.utilities.results.Result;
+import com.example.services.abstracts.LabelService;
+import com.example.services.dtos.requests.AddLabelRequest;
+import com.example.services.dtos.requests.UpdateLabelRequest;
+import com.example.services.dtos.responses.GetLabelResponse;
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/labels")
+@AllArgsConstructor
+@CrossOrigin
+public class LabelController {
+    private final LabelService labelService;
+
+    @GetMapping()
+    public DataResult<List<GetLabelResponse>> getAllLabels() {
+        return this.labelService.getAllLabels();
+    }
+
+    @GetMapping("/{id}")
+    public DataResult<GetLabelResponse> getById(@PathVariable int id) {
+        return this.labelService.getById(id);
+    }
+
+    @PostMapping()
+    @ResponseStatus(HttpStatus.CREATED)
+    public Result addLabel(@RequestBody AddLabelRequest addLabelRequest) {
+        return this.labelService.addLabel(addLabelRequest);
+    }
+
+    @PutMapping()
+    public Result updateLabel(@RequestBody @Valid UpdateLabelRequest updateLabelRequest) {
+        return this.labelService.updateLabel(updateLabelRequest);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public Result deleteLabel(@PathVariable int id) {
+        return this.labelService.deleteLabel(id);
+    }
+
 }
--- src/main/java/com/example/controllers/TaskController.java
+++ src/main/java/com/example/controllers/TaskController.java
@@ -1,2 +1,49 @@
-package com.example.controllers;public class TaskController {
+package com.example.controllers;
+
+import com.example.core.utilities.results.DataResult;
+import com.example.core.utilities.results.Result;
+import com.example.services.abstracts.TaskService;
+import com.example.services.dtos.requests.AddTaskRequest;
+import com.example.services.dtos.requests.UpdateTaskRequest;
+import com.example.services.dtos.responses.GetTaskResponse;
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/tasks")
+@AllArgsConstructor
+@CrossOrigin
+public class TaskController {
+
+    private final TaskService taskService;
+
+    @GetMapping()
+    public DataResult<List<GetTaskResponse>> getAllTasks() {
+        return this.taskService.getAllTasks();
+    }
+
+    @GetMapping("/{id}")
+    public DataResult<GetTaskResponse> getById(@PathVariable int id) {
+        return this.taskService.getById(id);
+    }
+
+    @PostMapping()
+    @ResponseStatus(HttpStatus.CREATED)
+    public Result addTask(@RequestBody AddTaskRequest addTaskRequest) {
+        return this.taskService.addTask(addTaskRequest);
+    }
+
+    @PutMapping()
+    public Result updateTask(@RequestBody @Valid UpdateTaskRequest updateTaskRequest) {
+        return this.taskService.updateTask(updateTaskRequest);
+    }
+
+    @DeleteMapping("/{id}")
+    public Result deleteTask(@PathVariable int id) {
+        return this.taskService.deleteTask(id);
+    }
 }
