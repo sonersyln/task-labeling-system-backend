5d8876bcec08e686ad3384c8a6c92fa431367e65
Soner <syln.soner@gmail.com>
2024-06-05 20:25:00 +0300

    Add findByUsername method to UserRepository

--- src/main/java/com/example/repositories/UserRepository.java
+++ src/main/java/com/example/repositories/UserRepository.java
@@ -1,2 +1,10 @@
-package com.example.repositories;public interface UserRepository {
+package com.example.repositories;
+
+import com.example.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+    Optional<User> findByUsername(String username);
 }
