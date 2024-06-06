0da652f6aa36ee0072317cafc64516bab57ee00f
Soner <syln.soner@gmail.com>
2024-06-06 14:14:47 +0300

    feat: Add CORS configuration for allowing cross-origin requests

--- src/main/java/com/example/core/config/CorsConfig.java
+++ src/main/java/com/example/core/config/CorsConfig.java
@@ -1,2 +1,18 @@
-package com.example.core.config;public class CorsConfig {
+package com.example.core.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE","PATCH", "HEAD", "OPTIONS")
+                .allowedHeaders("Content-Type", "Authorization");
+
+
+    }
 }
